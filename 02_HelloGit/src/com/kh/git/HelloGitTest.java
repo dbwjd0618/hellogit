6f249c7e6d3a8d0e7ca4127d54bcd89c46b7acd3
kp <kp@kp-PC>
2020-01-16 19:36:27 +0900

    remot repo 연결 후 첫 commit & push

--- 02_HelloGit/src/com/kh/git/HelloGitTest.java
+++ 02_HelloGit/src/com/kh/git/HelloGitTest.java
@@ -6,6 +6,7 @@ public class HelloGitTest {
 		System.out.println("안녕하세요!");
 		System.out.println("저녁맛있게 먹었어요?");
 		System.out.println("조정우바보");
+		System.out.println("축하드립니다. commit & push");
 	}
 
 }
